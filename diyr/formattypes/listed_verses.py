f991dca74d7aca0519633c83324706ed70eeb639
alextricity25 <miguel.can2@gmail.com>
2018-01-31 17:39:40 -0800

    Implemented listed_verses format type
    Listed verses allows the input file to be formatted in a
    list form.
    i.e.
    1. There is now then no condemnation to those who are in Christ Jesus,
    2. For the law of the Spirit of life has freed me from the law of
       sin and of death,
    etc..

---
+++ diyr/formattypes/listed_verses.py
@@ -0,0 +1,41 @@
+# =============================================================================
+# Copyright [2018] [Miguel Alex Cantu]
+# License Information :
+# This software has no warranty, it is provided 'as is'. It is your
+# responsibility to validate the behavior of the routines and its accuracy
+# using the code provided. Consult the GNU General Public license for further
+# details (see GNU General Public License).
+# http://www.gnu.org/licenses/gpl.html
+# =============================================================================
+
+from pyparsing import Word, alphas, OneOrMore, nums, Group
+from diyr.formattypes.base import BaseFormatClass
+
+class ListedVerses(BaseFormatClass):
+
+    def __init__(self, stream):
+    
+        # Grammers defined here
+        self.verse_word = Word( alphas + '.,?;')
+        self.verse = OneOrMore(self.verse_word)
+        # The verses for this formatter are listed numerically
+        # The identifier this formatter will use the number.
+        self.number = Word(nums + ".")
+        # This is the dataset we will mainly be operating on, since
+        # it will contain the verse and the identifier
+        self.verse_reference = Group(self.number) + Group(self.verse)
+        BaseFormatClass.__init__(self, stream)
+
+
+    # Must return a dataset with these 
+    def get_result(self):
+        # Reading input stream line by line
+        for line in self.stream:
+            parsed_line = self.verse_reference.parseString(line)
+            self.result['body'] = parsed_line[1]
+            # The identifier for each line when this file format is used
+            # is the prepended number (The verse number)
+            self.result['identifier'] = parsed_line[0]
+            # There are no extras for this formatter
+            self.result['extras'] = None
+            yield self.result
