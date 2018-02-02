660005fbf56a0feeab8a6721a5cb24956bbf7a68
alextricity25 <miguel.can2@gmail.com>
2018-02-01 16:20:36 -0800

    Update setup.py, move example files

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "diyr",
-    version = "0.0.1a2",
+    version = "0.0.1a3",
     author = "Miguel Alex Cantu",
     author_email = "miguel.can2@gmail.com	",
     description = ("An interactive program that hollows out text files"
@@ -22,9 +22,13 @@ setup(
         'diyr.formattypes',
         'diyr.engines'
     ],
+    install_requires = ['pyparsing'],
+    package_data = {
+        'diyr': ['example_format_types/listed_verses.txt']
+    },
     long_description=read('README.rst'),
     url = 'https://github.com/alextricity25/dwell_in_you_richly',
-    download_url = 'https://github.com/alextricity25/dwell_in_you_richly/archive/0.0.1a2.tar.gz',
+    download_url = 'https://github.com/alextricity25/dwell_in_you_richly/archive/0.0.1a3.tar.gz',
     entry_points = {
         'console_scripts': [
             'diyr = diyr.main:main'
